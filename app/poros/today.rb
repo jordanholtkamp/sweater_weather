82f1e98d50dced249bd2f9d3684e9df989bb8ef2
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-01 18:35:40 -0700

    Add tonight summary

--- app/poros/today.rb
+++ app/poros/today.rb
@@ -6,11 +6,16 @@ class Today
       low: data[:daily][:data][0][:temperatureMin],
       high: data[:daily][:data][0][:temperatureMax],
       # night_summary: data[:daily][:data][0][:summary] #needs to change
-      night_summary: self.tonight_summary(data[:daily][:data]) 
+      night_summary: self.tonight_summary(data[:hourly][:data]) 
     }
   end
 
   def self.tonight_summary(hourly_data)
+    night_hour = hourly_data.find do |hour|
+      Time.at(hour[:time]).in_time_zone('UTC').hour == 20
+    end
     require 'pry'; binding.pry
+    night_hour[:summary]
   end
 end
+# hour[:time]).in_time_zone('UTC').hour == 20
