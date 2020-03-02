0c5256eb66cefe849e2c2306b31545b97e84fcc1
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-02 10:06:00 -0700

    Add in travel time method for google service

--- app/services/google_geo_service.rb
+++ app/services/google_geo_service.rb
@@ -10,9 +10,15 @@ class GoogleGeoService
     "#{lat},#{long}"
   end
 
+  def travel_time
+    JSON.parse(directions_response.body, symbolize_names: true)[:routes][0][:legs][0][:duration][:text]
+  end
+
   def address
     parsed['results'][0]['formatted_address']
   end
+
+  private
   
   def conn
     Faraday.new("https://maps.googleapis.com")
@@ -25,5 +31,11 @@ class GoogleGeoService
 
   def parsed
     JSON.parse(response.body)
+  end
+
+  def directions_response
+    origin = @location.split(',')[0]
+    destination = @location.split(',')[2]
+    conn.get("/maps/api/directions/json?origin=#{origin}&destination=#{destination}&key=#{ENV['GOOGLE_API_KEY']}")
   end
 end
