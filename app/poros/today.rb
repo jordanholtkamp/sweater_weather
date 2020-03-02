11f464fbd4e2c7ca4e2d8b873f5c8d17f9e31a5d
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-01 18:35:50 -0700

    Remove comments

--- app/poros/today.rb
+++ app/poros/today.rb
@@ -5,7 +5,6 @@ class Today
       day_summary: data[:daily][:summary],
       low: data[:daily][:data][0][:temperatureMin],
       high: data[:daily][:data][0][:temperatureMax],
-      # night_summary: data[:daily][:data][0][:summary] #needs to change
       night_summary: self.tonight_summary(data[:hourly][:data]) 
     }
   end
@@ -14,8 +13,6 @@ class Today
     night_hour = hourly_data.find do |hour|
       Time.at(hour[:time]).in_time_zone('UTC').hour == 20
     end
-    require 'pry'; binding.pry
     night_hour[:summary]
   end
 end
-# hour[:time]).in_time_zone('UTC').hour == 20
