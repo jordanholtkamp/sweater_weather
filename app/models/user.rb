539bc8126dca938b9ba8b6c9512e240f87eaf725
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-03 08:54:53 -0700

    Add user resource

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password_digest, require: true
+  validates :password, confirmation: { case_sensitive: true }
+  validates :api_key, uniqueness: true, presence: true 
+
+  has_secure_password
+  has_secure_token :api_key
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_212255) do
+ActiveRecord::Schema.define(version: 2020_03_03_154500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_03_01_212255) do
     t.string "city"
     t.string "lat"
     t.string "long"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "api_key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
