d0114238f54925c1c952ad7ebe958df312e9eb15
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-03-04 09:04:44 -0700

    Authenticate password sheesh

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    if user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email])
+    if good_credentials(user)
       session[:user_id] = user.id
       render json: UserSerializer.new(user), status: 200
     else
       render json: {error: 'Bad credentials'}, status: 401
     end
+  end
+
+  private
+  def good_credentials(user)
+    user && user.authenticate(params[:password])
   end
 end
